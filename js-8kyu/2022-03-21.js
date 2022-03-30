52aeb74c4da4e6a17e56b90dcad7cf3d4bab183b
hiitsbl <97006407+blwalkerdavies@users.noreply.github.com>
2022-03-30 19:41:25 -0400

    Create 2022-03-21.js

---
+++ js-8kyu/2022-03-21.js
@@ -0,0 +1,38 @@
+// Rock Paper Scissors
+
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+
+
+const rps = (p1, p2) => {
+  if (p1 === 'paper') {
+    if (p2 === 'rock') {
+      return 'Player 1 won!'
+    } else {
+      if (p2 === 'scissors') {
+        return 'Player 2 won!'
+      }
+    }
+  }
+    if (p1 === 'scissors') {
+      if (p2 === 'rock') {
+        return 'Player 2 won!'
+      } else {
+        if (p2 === 'paper') {
+          return 'Player 1 won!'
+        }
+      }
+    }
+    if (p1 === 'rock') {
+      if (p2 === 'paper') {
+        return 'Player 2 won!'
+      } else {
+        if (p2 === 'scissors') {
+          return 'Player 1 won!'
+        }
+      }
+    }
+    if (p1 === p2) {
+      return 'Draw!'
+    }
+}
